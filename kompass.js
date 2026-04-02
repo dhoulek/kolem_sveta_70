de3848a2601b55b7b8809e219b157b1cb2d8deb9
hyni1999 <Hynek.Houlek@gmail.com>
2026-04-02 17:00:13 +0200

    Debug

--- kompass.js
+++ kompass.js
@@ -166,9 +166,9 @@
     if (e.webkitCompassHeading != null) {
       deviceHeading = e.webkitCompassHeading;                 // iOS
     } else if (e.absolute && e.alpha != null) {
-      deviceHeading = Math.round((360 - e.alpha) % 360);                 // Android absolute
+      deviceHeading = Math.round((360 - e.alpha) % 1080) * 3;                 // Android absolute
     } else if (e.alpha != null && deviceHeading === null) {
-      deviceHeading = Math.round((360 - e.alpha) % 360);                 // fallback
+      deviceHeading = Math.round((360 - e.alpha) % 1080) * 3;                 // fallback
     }
   }
 
